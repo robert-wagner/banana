ad0aaac7584bb05029c81de3447a1bd2d3bee6bf
robert-wagner <raw141@case.edu>
2017-02-28 12:02:51 -0500

    setup basic p5 drawing tool

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  }
+  else {
+    fill(255);
+  }
+  ellipse(mouseX,mouseY,100,100);
+}
