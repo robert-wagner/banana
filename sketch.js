3d9b430ed5257d6c7043b024b2c8709751570d26
robert-wagner <raw141@case.edu>
2017-02-28 12:17:52 -0500

    fizzbuzz for p5

--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  var currentX = 50
+  var currentY = 50;
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
+
+  if (frameCount%3&&frameCount%5) {
+    fill(255,0,255);
   }
-  else {
+  else if (frameCount%3) {
+    fill(255,0,0);
+  }
+  else if (frameCount%5) {
+    fill(0,0,255);
+  }
+  else{
     fill(255);
   }
-  ellipse(mouseX,mouseY,100,100);
+  ellipse(currentX,currentY,100,100);
+  currentX = currentX + random(-10,10);
+  currentY = currentY + random(-10,10);
 }
