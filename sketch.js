89cdddd775327b4c0ec3c6a56950a76926b0b398
robert-wagner <raw141@case.edu>
2017-03-01 00:31:38 -0500

    updated to seek food

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var currentX = 0;
 var currentY = 0;
+var foodFlag = 0;
+var foodX = 0;
+var foodY = 0;
 function setup() {
   createCanvas(windowWidth,windowHeight);
   currentX = width/2;
@@ -21,6 +24,19 @@ function draw() {
     fill(255);
   }
   ellipse(currentX,currentY,100,100);
-  currentX = currentX + random(-10,10);
-  currentY = currentY + random(-10,10);
+  if (foodFlag) {
+    if (dist(currentX,currentY,foodX,foodY)<20) {
+      foodFlag = 0;
+    } else {
+      currentX = currentX + random(-10,10) + 5*cos(atan2(foodY-currentY,foodX-currentX));
+      currentY = currentY + random(-10,10)+ 5*sin (atan(foodY-currentY,foodX-currentX));
+    }
+  } else {
+    foodX = random(0,width);
+    foodY = random(0,height);
+    fill(255,255,0);
+    ellipse(foodX,foodY,20,20);
+    foodFlag = 1;
+  }
+
 }
