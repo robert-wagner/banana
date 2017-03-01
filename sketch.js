74dee0c35dfb5c3c165b163ed5110656ecaa9f73
robert-wagner <raw141@case.edu>
2017-03-01 00:45:02 -0500

    fixed color pallet, moddified food finding

--- sketch.js
+++ sketch.js
@@ -11,25 +11,21 @@ function setup() {
 
 function draw() {
 
-  if (frameCount%3&&frameCount%5) {
-    fill(255,0,255);
-  }
-  else if (frameCount%3) {
-    fill(255,0,0);
-  }
-  else if (frameCount%5) {
-    fill(0,0,255);
-  }
-  else{
-    fill(255);
-  }
+  fill(currentX/width*255,currentY/height*255,128);
   ellipse(currentX,currentY,100,100);
   if (foodFlag) {
     if (dist(currentX,currentY,foodX,foodY)<20) {
       foodFlag = 0;
-    } else {
-      currentX = currentX + random(-10,10) + 5*cos(atan2(foodY-currentY,foodX-currentX));
-      currentY = currentY + random(-10,10)+ 5*sin (atan(foodY-currentY,foodX-currentX));
+    }
+    else {
+      if (dist(currentX,currentY,foodX,foodY)>(height+width)/5) {
+        currentX = currentX + random(-10,10);
+        currentY = currentY + random(-10,10);
+      }
+      else {
+        currentX = currentX + random(-10,10) + 5*cos(atan2(foodY-currentY,foodX-currentX));
+        currentY = currentY + random(-10,10)+ 5*sin (atan(foodY-currentY,foodX-currentX));
+      }
     }
   } else {
     foodX = random(0,width);
