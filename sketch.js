81764cee7430575eb3b975fabae8049ec290c547
Robert (Bobby) Wagner <raw141@case.edu>
2018-03-25 13:44:53 -0400

    inital work on banana

--- sketch.js
+++ sketch.js
@@ -4,42 +4,63 @@ var foodFlag = 0;
 var foodX = 0;
 var foodY = 0;
 var sizeEllipse = 100;
+var totalIterations = 1000
+var initialPositions = NaN
+var finalPostions = NaN
+var live = false
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  currentX = width/2;
-  currentY = height/2;
+  createCanvas(windowWidth, windowHeight);
+  text("for kjk, Coming Soon")
+  sleep(2)
+  clear()
+  //initial banana drawing here
+  var currentPositions = initialPositions
+
 }
 
 function draw() {
-
-  fill(currentX/width*255,currentY/height*255,128);
-  noStroke();
-  ellipse(currentX,currentY,sizeEllipse,sizeEllipse);
-  if (foodFlag) {
-    if (dist(currentX,currentY,foodX,foodY)<20) {
-      foodFlag = 0;
-      sizeEllipse +=20;
+  if (live) {
+    if (currentIteration < totalIterations) {
+      for (var i = 0; i < currentPositions.length; i++) {
+        currentPositions[i][0] = currentPositions[i][0] + (dist(currentPositions[i][0], 0, finalPostions[i][0], 0) / (totalIterations - currentIteration)) * (finalPostions[i][0] - currentPositions[i][0])
+      }
+      currentIteration++;
     }
-    else {
-      if ((dist(currentX,currentY,foodX,foodY)>(height+width)/5)&&(frameCount%7!=0)) {
-        currentX = currentX + random(-10,10);
-        currentY = currentY + random(-10,10);
-        sizeEllipse -=(1/20);
-        sizeEllipse = constrain(sizeEllipse,50,200);
+    fill(currentX / width * 255, currentY / height * 255, 128);
+    noStroke();
+    ellipse(currentX, currentY, sizeEllipse, sizeEllipse);
+    if (foodFlag) {
+      if (dist(currentX, currentY, foodX, foodY) < 20) {
+        foodFlag = 0;
+        sizeEllipse += 20;
       }
       else {
-        currentX = currentX + random(-10,10) + 5*cos(atan2(foodY-currentY,foodX-currentX));
-        currentY = currentY + random(-10,10)+ 5*sin (atan(foodY-currentY,foodX-currentX));
-        sizeEllipse -=(1/20);
-        sizeEllipse = constrain(sizeEllipse,50,200);
+        if ((dist(currentX, currentY, foodX, foodY) > (height + width) / 5) && (frameCount % 7 != 0)) {
+          currentX = currentX + random(-10, 10);
+          currentY = currentY + random(-10, 10);
+          sizeEllipse -= (1 / 20);
+          sizeEllipse = constrain(sizeEllipse, 50, 200);
+        }
+        else {
+          currentX = currentX + random(-10, 10) + 5 * cos(atan2(foodY - currentY, foodX - currentX));
+          currentY = currentY + random(-10, 10) + 5 * sin(atan(foodY - currentY, foodX - currentX));
+          sizeEllipse -= (1 / 20);
+          sizeEllipse = constrain(sizeEllipse, 50, 200);
+        }
       }
+    } else {
+      foodX = random(0, width);
+      foodY = random(0, height);
+      //fill(255,255,0);
+      //ellipse(foodX,foodY,20,20);
+      foodFlag = 1;
     }
-  } else {
-    foodX = random(0,width);
-    foodY = random(0,height);
-    //fill(255,255,0);
-    //ellipse(foodX,foodY,20,20);
-    foodFlag = 1;
   }
 
+
+}
+function drawCurrent(currentPositions) {
+  for (var i = 0; i < currentPositions.length; i++) {
+
+  }
 }
